4d7beac445f82d549b7513fdb003b15a56a724a8
giuliacrr <giulia.corrado02@gmail.com>
2023-10-31 17:46:15 +0100

    Basic changes

--- database/migrations/2023_10_31_155426_create_apartments_table.php
+++ database/migrations/2023_10_31_155426_create_apartments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
+
+
+            
             $table->timestamps();
         });
     }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,2 @@
-import './bootstrap';
 import '~resources/scss/app.scss';
-import * as bootstrap from 'bootstrap';
-import.meta.glob([
-    '../img/**'
-])
+import "bootstrap/dist/js/bootstrap.js";
