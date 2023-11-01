65a0390b585f22b253e0155558062cdaa1464052
Pietro-Leone <leonepietro1999@gmail.com>
2023-11-01 12:08:38 +0100

    add slug

--- database/migrations/2023_10_31_155426_create_apartments_table.php
+++ database/migrations/2023_10_31_155426_create_apartments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string("title");
+            $table->string("slug");
             $table->integer("rooms_number");
             $table->integer("beds_number");
             $table->integer("bathrooms_number");
