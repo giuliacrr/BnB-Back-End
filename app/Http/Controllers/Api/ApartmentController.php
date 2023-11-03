a53c7481b7f3bcd16a289ef36c42236bdafb441c
Pietro-Leone <leonepietro1999@gmail.com>
2023-11-03 15:39:30 +0100

    added Api

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
   public function index()
   {
-    
+    $apartments = Apartment::all();
+
+    return response()->json($apartments);
+  }
+
+  public function show($slug)
+  {
+    $apartment = Apartment::where("slug", $slug)
+      ->with(['user','views','sponsorships','services'])
+      ->first();
+
+      return response()->json($apartment);
   }
 }
