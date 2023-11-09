ac543448b6accf4446ca2d03b58d9987a51bfc2c
ElyPoli <itsos.poli@gmail.com>
2023-11-09 12:54:16 +0100

    add link in dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,26 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+    <div class="container">
+        <h1 class="fs-2 my-4">
+            {{ Auth::user()->name ?? 'User' }} ecco la tua Dashboard!
+        </h1>
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="dashboard-header">
+                    <div class="card">
+                        <div class="card-header">
+                            Dashboard
+                        </div>
+                        <div class="card-body">
+                            <div class="dashboard-box">
+                                <div class="row row-cols-1 row-cols-md-3 gy-3 justify-content-center">
+                                    {{-- Visualizza tutte le tue strutture --}}
+                                    <div class="col">
+                                        <div class="d-flex justify-content-center h-100">
+                                            <div class="card">
+                                                <a class="card-title" href="{{ Route('admin.apartments.index') }}">
+                                                    Visualizza tutte le tue strutture
+                                                </a>
+                                                <p class="card-content">
+                                                    Esplora la tua lista di strutture.
+                                                </p>
+                                                <div class="card-arrow">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" height="15" width="15">
+                                                        <path fill="#fff"
+                                                            d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z">
+                                                        </path>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {{-- Aggiungi una nuova struttura --}}
+                                    <div class="col">
+                                        <div class="d-flex justify-content-center h-100">
+                                            <div class="card">
+                                                <a class="card-title" href="{{ Route('admin.apartments.create') }}">
+                                                    Aggiungi una nuova struttura
+                                                </a>
+                                                <p class="card-content">
+                                                    Rendi il tuo spazio un'esperienza: fai il primo passo ora.
+                                                </p>
+                                                <div class="card-arrow">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" height="15" width="15">
+                                                        <path fill="#fff"
+                                                            d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z">
+                                                        </path>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {{-- Visualizza i messaggi relativi agli appartamenti --}}
+                                    <div class="col">
+                                        <div class="d-flex justify-content-center h-100">
+                                            <div class="card">
+                                                <a class="card-title" href="{{ Route('admin.apartments.create') }}">
+                                                    Visualizza tutti i messaggi
+                                                </a>
+                                                <p class="card-content">
+                                                    Consulta la tua bacheca dei messaggi.
+                                                </p>
+                                                <div class="card-arrow">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" height="15" width="15">
+                                                        <path fill="#d6d3c8"
+                                                            d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z">
+                                                        </path>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
