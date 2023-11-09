96cf5873e63393136bda04545c7cac94ac845673
Pietro-Leone <leonepietro1999@gmail.com>
2023-11-09 10:12:03 +0100

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(ApartmentSeeder::class);
+        $this->call(ServicesTableSeeder::class);
+        $this->call(SponsorshipsTableSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
 }
