7587788968e9812a2f24899cfa1a833b181eb33d
Pietro-Leone <leonepietro1999@gmail.com>
2023-11-15 00:51:27 +0100

    removed welcome

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('guest.welcome');
+    return redirect("http://localhost:5174/");
 });
 
 Route::get('/dashboard', function () {
